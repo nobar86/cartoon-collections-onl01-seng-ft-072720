71b51c569eb8b50c77c71ad362bf73504fee91c7
Michael Rabon <nobar86@gmail.com>
2020-08-04 17:01:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,11 @@ end
 
 def long_planeteer_calls(array)
   i = 0 
-  if array.any? 
+  if array.any? {|i| i.length > 4}
+    return true
+  else
+    return false
+  end
 end
 
 def find_the_cheese
